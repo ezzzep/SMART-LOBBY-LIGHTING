0255a276ae8e8966e16332e220c8fb4213b9c0fc
joseph <joseph@gmail.com>
2025-02-12 20:27:24 +0800

    adjusted appbar

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -54,19 +54,6 @@ class _LoginState extends State<Login> {
       backgroundColor: Colors.transparent,
       elevation: 0,
       toolbarHeight: 100,
-      leading: GestureDetector(
-        onTap: () => Navigator.pop(context),
-        child: Container(
-          margin: const EdgeInsets.only(left: 10),
-          decoration: const BoxDecoration(
-            color: Color(0xffF7F7F9),
-            shape: BoxShape.circle,
-          ),
-          child: const Center(
-            child: Icon(Icons.arrow_back_ios_new_rounded, color: Colors.black),
-          ),
-        ),
-      ),
     );
   }
 
